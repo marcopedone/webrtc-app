74b124f16d807c8f8e7d48e3cbca556e9e6a8211
Marco Pedone <m.pedone98@gmail.com>
2026-07-17 13:02:23 +0200

    configs

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,11 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html', // Necessario per gestire i parametri della query (?join=...) senza errori 404
+			precompress: false,
+			strict: true
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
